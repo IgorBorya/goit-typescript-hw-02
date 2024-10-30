f860d060b02b137c1eabce64ea499671c4b5e757
Ihor_Haraniuk <igorgaranyuk133@gmail.com>
2024-10-30 15:48:41 +0200

    remove consolelog

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import s from "./ImageCard.module.css";
 
 const ImageCard = ({ image, onClick }) => {
-  console.log(image);
   return (
     <div className={s.imageCard} onClick={onClick}>
       <img src={image.urls.thumb} alt={image.tags} />
