8ba6bd7b5be439d8e1151ebfc88d06027d90ed3f
Ihor_Haraniuk <igorgaranyuk133@gmail.com>
2024-10-30 16:34:06 +0200

    init project and add base config

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from "./App";
 import { Toaster } from "react-hot-toast";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
     <Toaster />
